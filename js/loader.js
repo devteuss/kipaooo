19c5a53edb64053539025d13974c2ed976b35b4a
devteuss <cielodavinci@gmail.com>
2026-01-21 20:39:52 -0300

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,3 +1,12 @@
 window.addEventListener("load", () => {
-  document.getElementById("loader").style.display = "none";
+  const skeleton = document.getElementById("skeleton");
+
+  setTimeout(() => {
+    skeleton.style.opacity = "0";
+    skeleton.style.pointerEvents = "none";
+
+    setTimeout(() => {
+      skeleton.remove();
+    }, 500);
+  }, 1200);
 });
